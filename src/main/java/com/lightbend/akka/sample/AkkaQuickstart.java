710ea0eb23a1a70fa9cc27dc99c2d3213bb026df
sanasov <sanasov@phoenixit.ru>
2018-06-27 19:35:48 +0300

    randomly change text message for testing

--- src/main/java/com/lightbend/akka/sample/AkkaQuickstart.java
+++ src/main/java/com/lightbend/akka/sample/AkkaQuickstart.java
@@ -9,39 +9,35 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 
 public class AkkaQuickstart {
-  public static void main(String[] args) {
-    final ActorSystem system = ActorSystem.create("helloakka");
-    try {
-      //#create-actors
-      final ActorRef printerActor = 
-        system.actorOf(Printer.props(), "printerActor");
-      final ActorRef howdyGreeter = 
-        system.actorOf(Greeter.props("Howdy", printerActor), "howdyGreeter");
-      final ActorRef helloGreeter = 
-        system.actorOf(Greeter.props("Hello", printerActor), "helloGreeter");
-      final ActorRef goodDayGreeter = 
-        system.actorOf(Greeter.props("Good day", printerActor), "goodDayGreeter");
-      //#create-actors
+    public static void main(String[] args) {
+        final ActorSystem system = ActorSystem.create("helloakka");
+        try {
+            //#create-actors
+            final ActorRef printerActor = system.actorOf(Printer.props(), "printerActor");
+            final ActorRef howdyGreeter = system.actorOf(Greeter.props("message 1", printerActor), "howdyGreeter");
+            final ActorRef helloGreeter = system.actorOf(Greeter.props("message 2", howdyGreeter), "helloGreeter");
+            final ActorRef goodDayGreeter = system.actorOf(Greeter.props("message 3", printerActor), "goodDayGreeter");
+            //#create-actors
 
-      //#main-send-messages
-      howdyGreeter.tell(new WhoToGreet("Akka"), ActorRef.noSender());
-      howdyGreeter.tell(new Greet(), ActorRef.noSender());
+            //#main-send-messages
+            howdyGreeter.tell(new WhoToGreet("Akka"), ActorRef.noSender());
+            howdyGreeter.tell(new Greet(), ActorRef.noSender());
 
-      howdyGreeter.tell(new WhoToGreet("Lightbend"), ActorRef.noSender());
-      howdyGreeter.tell(new Greet(), ActorRef.noSender());
+            howdyGreeter.tell(new WhoToGreet("Lightbend"), ActorRef.noSender());
+            howdyGreeter.tell(new Greet(), ActorRef.noSender());
 
-      helloGreeter.tell(new WhoToGreet("Java"), ActorRef.noSender());
-      helloGreeter.tell(new Greet(), ActorRef.noSender());
+            helloGreeter.tell(new WhoToGreet("Java"), ActorRef.noSender());
+            helloGreeter.tell(new Greet(), ActorRef.noSender());
 
-      goodDayGreeter.tell(new WhoToGreet("Play"), ActorRef.noSender());
-      goodDayGreeter.tell(new Greet(), ActorRef.noSender());
-      //#main-send-messages
+            goodDayGreeter.tell(new WhoToGreet("Play"), ActorRef.noSender());
+            goodDayGreeter.tell(new Greet(), ActorRef.noSender());
+            //#main-send-messages
 
-      System.out.println(">>> Press ENTER to exit <<<");
-      System.in.read();
-    } catch (IOException ioe) {
-    } finally {
-      system.terminate();
+            System.out.println(">>> Press ENTER to exit <<<");
+            System.in.read();
+        } catch (IOException ioe) {
+        } finally {
+            system.terminate();
+        }
     }
-  }
 }
